4fb2d273dc61ace0e1a2b7a7e523a2ec8faf8fa3
Chinmaybhake29 <125784199+Chinmaybhake29@users.noreply.github.com>
2025-05-23 20:16:15 +0530

    Add files via upload

--- News App/script.js
+++ News App/script.js
@@ -1,50 +1,46 @@
-const API_KEY = "f587be63cb774c1884498ee7a80d864d";
-const URL = "https://newsapi.org/v2/everything?q=";
+const API_KEY = "32768006ae1f0c0e665eda4705aab7ac"; // Replace with your real GNews API key
+const URL = "https://gnews.io/api/v4/search?q=";
 
 window.addEventListener("load", () => fetchNews("Headlines"));
 
 async function fetchNews(query) {
-  // console.log(query);
-
-  let res = await fetch(`${URL}${query}&from=2024-11-10&apiKey=${API_KEY}`);
-  let data = await res.json();
-  console.log(data);
-  console.log(data.articles);
-  bindNews(data.articles);
+  try {
+    let res = await fetch(`${URL}${query}&lang=en&apikey=${API_KEY}`);
+    let data = await res.json();
+    console.log(data);
+    bindNews(data.articles);
+  } catch (err) {
+    console.log("Error fetching news:", err);
+  }
 }
 
 const bindNews = (articles) => {
   if (articles.length > 0) {
-    var str = "";
+    let str = "";
     articles.forEach((article) => {
       str += `
-            <div class="col-xl-4 news-card">
-                <div class="card">
-                    <img src="${article.urlToImage}"class="card-img-top" alt="Image" />
-                    <div class="card-body">
-                    <h4 class="card-title">${article.title}</h4>
-                    <h6>${article.source.name} 📢 ${article.publishedAt}</h6>
-                    <p class="card-text">
-                        ${article.description}</p>
-                    </p>
-                    <a href="${article.url}" class="btn btn-primary" target="_blank">Read More</a>
-                    </div>
-                </div>
+        <div class="col-xl-4 news-card">
+          <div class="card">
+            <img src="${article.image}" class="card-img-top" alt="Image" />
+            <div class="card-body">
+              <h4 class="card-title">${article.title}</h4>
+              <h6>${article.source.name} 📢 ${article.publishedAt}</h6>
+              <p class="card-text">${article.description}</p>
+              <a href="${article.url}" class="btn btn-primary" target="_blank">Read More</a>
             </div>
-            `;
-      document.querySelector(".row").innerHTML = str;
+          </div>
+        </div>`;
     });
+    document.querySelector(".row").innerHTML = str;
   }
 };
 
 async function fetchCricketNews() {
   await fetchNews("cricket");
 }
-
 async function fetchAutomobileNews() {
   await fetchNews("automobile");
 }
-
 async function fetchStockNews() {
   await fetchNews("stocks");
 }
